1585c316f22a7f37c91540526133767f8494a34e
modenapclocal <modena3d@gmail.com>
2024-05-12 14:40:26 -0300

    display none

--- 02---QUAL_COR/js/script.js
+++ 02---QUAL_COR/js/script.js
@@ -1,12 +1,42 @@
-const colors=['green', 'red', 'rgba(133, 122, 200)', '#f15025'];
+const colorsNomes=['#BC8F8F', 'rgba(176,224,230)', 'rgba(128,0,0)', '#rgba(199,21,133)'];
+const colorsExa=['#BC8F8F', '4B0082', '#B22222', '	#FFD700'];
+const colorsRGB=['#BC8F8F', 'rgba(176,224,230)', 'rgba(128,0,0)', '#rgba(199,21,133)'];
+const colorsMix=['green', 'red', 'rgba(133, 122, 200)', '#f15025'];
 
-const btn = document.getElementById('btn')
+//-------------------BUTTONS--------------------------//
+let btnNomes = document.getElementById('btnNomes')
+let btnExa = document.getElementById('btnExa')
+let btnRGB = document.getElementById('btnRGB')
+let btnMix = document.getElementById('btnMix')
+//-------------------Visible--------------------------//
+//btnNomes.style.visibility = 'hidden'
+    btnNomes.style.display= 'none';
+    btnExa.style.display= 'none';
+    btnRGB.style.display= 'none';
+    btnMix.style.display = 'none';    
+
+//-------------------Menu--------------------------//
+const linkMenuNomes = document.querySelector('#linkNomes')
+linkMenuNomes.addEventListener('click', function(){
+    //alert('clicou');
+    btnNomes.style.display= 'initial';
+    btnExa.style.display= 'none';
+    btnRGB.style.display= 'none';
+    btnMix.style.display = 'none';        
+})
+
+
+
+//-------------------Text--------------------------//
 const textColor = document.querySelector('.span-color')
 
-btn.addEventListener('click', function(){
-    console.log('clicou')
-    const randomNumber = Math.floor(Math.random() * 4+1 )
 
-    document.body.style.backgroundColor = colors[randomNumber]
-    textColor.textContent = colors[randomNumber]
+btnMix.addEventListener('click', function(){
+    console.log('clicou')
+    const randomNumber = Math.floor(Math.random() * colorsMix.length )
+    //backgorund-color
+    const mainColor = document.querySelector('#main-color')
+    mainColor.style.backgroundColor = colorsMix[randomNumber]
+    //Text
+    textColor.textContent = colorsMix[randomNumber]
 })
